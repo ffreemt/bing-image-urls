d8c99f9206ff9990a951a0f75c490e162644c06f
ffreemt <yucongo+fmt@gmail.com>
2020-10-02 18:08:00 +0800

    first commit

---
+++ bing_image_urls/__init__.py
@@ -0,0 +1,2 @@
+__version__ = '0.1.0'
+from .bing_image_urls import bing_image_urls
---
+++ tests/test_bing_image_urls.py
@@ -0,0 +1,5 @@
+from bing_image_urls import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
