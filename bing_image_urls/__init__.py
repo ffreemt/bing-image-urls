9ed9c67443259fe9803196c11ab3b800f2c70c5a
freemt <yucongo+fmt@gmail.com>
2021-06-13 15:42:31 +0800

    Bump version from 0.1.3 to 0.1.4, filters in kwargs and other minor fixes

--- bing_image_urls/__init__.py
+++ bing_image_urls/__init__.py
@@ -1,4 +1,9 @@
 """ __init__. """
 __version__ = "0.1.3"
-from .bing_image_urls import bing_image_urls  # NOQA: F401
-from .get_image_size import get_image_size  # NOQA: F401
+from .bing_image_urls import bing_image_urls
+from .get_image_size import get_image_size
+
+__all__ = [
+    "bing_image_urls",
+    "get_image_size",
+]
